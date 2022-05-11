7cfe4bdb5e64c651cb349a749ccbb66c81c480c9
Jonas Tolentino <jonasmarco.ti@gmail.com>
2022-05-11 17:55:07 -0300

    fix: add default theme config

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -4,5 +4,57 @@ export default {
     gutter: '3.2rem',
     margin: '0 auto',
     padding: '0 1rem'
+  },
+  border: {
+    radius: '1rem',
+    full: '5rem'
+  },
+  font: {
+    family:
+      "Barlow Condensed, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif",
+    secondFamily:
+      "Poppins, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif",
+    light: 300,
+    normal: 400,
+    bold: 700,
+    sizes: {
+      xsmall: '1.2rem',
+      small: '1.4rem',
+      medium: '1.8rem',
+      large: '2.2rem',
+      xlarge: '2.8rem',
+      xxlarge: '3.2rem',
+      xxxlarge: '4.2rem',
+      xtreme: '6.2rem'
+    }
+  },
+  colors: {
+    primary: '#5B31E1',
+    secondary: '#8737E1',
+    mainBg: '#1E1923',
+    white: '#ffffff',
+    gray: '#B4B0B9',
+    lightGray: '#5E5A62',
+    darkGray: '#27212D',
+    darkPurple: '#211B26'
+  },
+  buttons: {
+    radius: '50rem'
+  },
+  spacings: {
+    xxsmall: '0.8rem',
+    xsmall: '1.6rem',
+    small: '2.4rem',
+    medium: '3.0rem',
+    large: '4.2rem',
+    xlarge: '4.8rem',
+    xxlarge: '5.6rem'
+  },
+  layers: {
+    base: 10,
+    menu: 20,
+    overlay: 30,
+    modal: 40,
+    alwaysOnTop: 50
   }
 }
